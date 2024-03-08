8de1102f8d48834643cb55e7b09a7cb09e7e0e60
rinkesh375 <rinkeshujjwal16@gmail.com>
2024-03-08 17:26:57 +0530

    global 404 and specific 404 page

---
+++ src/app/about/[...about_specific-404]/page.js
@@ -0,0 +1,9 @@
+const About_Specific_404 = ()=>{
+     return(
+        <h1>About Specific 404 Page Not Found</h1>
+     )
+
+
+}
+
+export default About_Specific_404;
---
+++ src/app/not-found.js
@@ -0,0 +1,10 @@
+const Global_404_Page = ()=>{
+
+return (<>
+  <h1>404 Page Not Found</h1>
+</>)
+
+}
+
+
+export default Global_404_Page;
