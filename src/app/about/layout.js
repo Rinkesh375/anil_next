2c1c538ad011d523eecb6b1481adf3f0af675ffa
rinkesh375 <rinkeshujjwal16@gmail.com>
2024-03-08 16:03:54 +0530

    conditional layout rendering

--- src/app/about/layout.js
+++ src/app/about/layout.js
@@ -1,8 +1,11 @@
+"use client";
+import { usePathname } from "next/navigation"
 export default function LayoutAbout ({children}){
-
+const currentPath = usePathname();
 return (
     <>
-    <h1>This Layout Will Be Available In All The About Component No Matter How Depth Your Component Are In About Folder</h1>
+    {/* This is called condtion layout rendering */}
+     {currentPath !== "/about" &&  <h1>This Layout Will Be Available In All The About Component No Matter How Depth Your Component Are In About Folder</h1>}
     
      <div>
         
