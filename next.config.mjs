3e280a3efc564a84744ddc14934f4845300de13b
rinkesh375 <rinkesh@epesign.com>
2024-03-19 19:43:45 +0530

    dynamic redirection implement

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    redirects:async()=>{
+        return [
+            {
+                source:"/user",
+                destination:"/login",
+                permanent:false
+            },
+            {
+                source:"/admin",
+                destination:"/login",
+                permanent:false
+            },
+            {
+               source:"/user/:userId" ,
+               destination:"/login",
+               permanent:false
+            }
+        ]
+    }
+};
 
 export default nextConfig;
