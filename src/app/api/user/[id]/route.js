1698a5fa57b5e3b83d71ad96152d48707fb54345
rinkesh375 <rinkesh@epesign.com>
2024-03-21 00:54:14 +0530

    made a put-request in [id]

--- src/app/api/user/[id]/route.js
+++ src/app/api/user/[id]/route.js
@@ -5,3 +5,11 @@ export function GET(_,content){
 const singleUser = users.filter((item)=>item.id === +content.params.id);
 return NextResponse.json(singleUser.length?{result:singleUser[0],success:true,stauts:200}:{result:"No Data Found"})
 }
+
+
+export  async function PUT(request,content){
+    const {name} = await request.json()
+    const {id} = content.params
+     console.log(name)
+    return NextResponse.json({result:"PUT Request Made Successfully",success:true,stauts:200})
+}
